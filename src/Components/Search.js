1ea991874e7bdfd6eca1584beb8dc6e5029a8496
Anton Parkhimovich <pad1907@gmail.com>
2021-11-12 02:36:34 +0300

    feat: add search code

---
+++ src/Components/Search.js
@@ -0,0 +1,8 @@
+export const Search =()=>{
+    return(
+        <div className={'search'}>
+            <input type={'text'} placeholder={'введите город'}/>
+            <button className={'search-button'}>Поиск</button>
+        </div>
+    )
+}
